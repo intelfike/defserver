9c1f1dc5a0651c1346b5087870174686c531db92
intelfike <intelfike@gmail.com>
2017-07-20 21:13:02 +0900

    更新済みのみ送信するよう変更

--- main.go
+++ main.go
@@ -4,20 +4,30 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+
+	"github.com/intelfike/checkmodfile"
 )
 
 var port = flag.String("http", ":8888", "HTTP port number.")
 
 func init() {
+	f, err := checkmodfile.RegistFile("main.go")
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		r.ParseForm()
-		value := r.FormValue("key")
-		fmt.Println(value)
-		w.Write([]byte(value))
+		b, err := f.GetLatest()
+		if err != nil {
+			fmt.Fprint(w, err)
+			return
+		}
+		w.Write(b)
 	})
 }
 
 func main() {
-	fmt.Println("Start HTTP Server localhost:", *port)
+	fmt.Printf("Start HTTP Server localhost%s", *port)
 	fmt.Println(http.ListenAndServe(*port, nil))
 }
